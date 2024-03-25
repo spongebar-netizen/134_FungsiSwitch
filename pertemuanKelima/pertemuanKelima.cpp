4187e04046504366b47df5dd25dfbb916bcd294b
spongebar-netizen <fatanjrz1@gmail.com>
2024-03-25 10:08:41 +0700

    Add project files.

---
+++ pertemuanKelima/pertemuanKelima.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int a, b;
+
+void input()
+{
+    cout << "Bilangan Pertama = ";
+    cin >> a;
+
+    cout << "Bilangan Kedua = ";
+    cin >> b;
+}
+
+int Penjumlahan(int bil_1, int bil_2) {
+    return bil_1 + bil_2;
+}
+
+int Pengurangan(int bil_1, int bil_2) {
+    return bil_1 - bil_2;
+}
+
+int Perkalian(int bil_1, int bil_2) {
+    return bil_1 * bil_2;
+}
+
+int Pembagian(int bil_1, int bil_2) {
+    return bil_1 / bil_2;
+}

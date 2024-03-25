ec62540ce7f3f18b49b71cbc5b018faedf461206
spongebar-netizen <fatanjrz1@gmail.com>
2024-03-25 10:39:59 +0700

    } while (pilihan != 5);

--- pertemuanKelima/pertemuanKelima.cpp
+++ pertemuanKelima/pertemuanKelima.cpp
@@ -32,40 +32,52 @@ int Pembagian(int bil_1, int bil_2) {
 int main()
 {
     int pilihan;
-
-    system("cls");
-    cout << "======= Menu ======" << endl;
-    cout << "1. Penjumlahan" << endl;
-    cout << "2. Pengurangan" << endl;
-    cout << "3. Perkalian" << endl;
-    cout << "4. Pembagian" << endl;
-    cout << "5.Keluar" << endl;
-    cout << "Masukkan menu yang ingin dipilih (1/2/3/4/5): ";
-    cin >> pilihan;
-
-
-    switch (pilihan)
+    do
     {
-    case 1:
-        input();
-        cout << "hasil perjumlahan = " << Penjumlahan (a, b) << endl;
-        system("pause");
-        break;
 
-    case 2:
-        input();
-        cout << "hasil pengurangan = " << Pengurangan (a, b) << endl;
-        system("pause");
-        break;
-    case 3:
-        input();
-        cout << "hasil perkalian = " << Perkalian (a, b) << endl;
-        system("pause");
-        break;
-    case 4:
-        input();
-        cout << "hasil pembagian = " << Pembagian (a, b) << endl;
-        system("pause");
-        break;
-    case 5:
-        break
+        system("cls");
+        cout << "======= Menu ======" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5.Keluar" << endl;
+        cout << "Masukkan menu yang ingin dipilih (1/2/3/4/5): ";
+        cin >> pilihan;
+
+
+        switch (pilihan)
+        {
+        case 1:
+            input();
+            cout << "hasil perjumlahan = " << Penjumlahan(a, b) << endl;
+            system("pause");
+            break;
+
+        case 2:
+            input();
+            cout << "hasil pengurangan = " << Pengurangan(a, b) << endl;
+            system("pause");
+            break;
+        case 3:
+            input();
+            cout << "hasil perkalian = " << Perkalian(a, b) << endl;
+            system("pause");
+            break;
+        case 4:
+            input();
+            cout << "hasil pembagian = " << Pembagian(a, b) << endl;
+            system("pause");
+            break;
+        case 5:
+            break;
+        default:
+            cout << "Menu tidak tersedia silahkan memlilih lagi !!!!!!!!" << endl;
+            break;
+        }
+
+    } while (pilihan != 5);
+
+
+}
+
